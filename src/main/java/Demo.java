1f9cc910886da13230c58d8c29b3c4f14fbce5e3
2022111744 <2740467350@qq.com>
2024-10-29 11:18:31 +0800

    first

---
+++ src/main/java/Demo.java
@@ -0,0 +1,20 @@
+import io.shardingsphere.shardingjdbc.api.yaml.YamlShardingDataSourceFactory;
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Demo {
+    public static void main(String[] args) throws SQLException, IOException {
+        File conf = new File("./src/main/resources/conf.yml");
+        DataSource dataSource = YamlShardingDataSourceFactory.createDataSource(conf);
+        Connection conn = dataSource.getConnection();
+        Statement stmt = conn.createStatement();
+
+        System.out.println(stmt.executeUpdate("insert into users(id, name) values(1, 'zhangsan')"));
+        System.out.println(stmt.executeUpdate("insert into item(id, value) values(1, 100)"));
+        System.out.println(stmt.executeUpdate("insert into item(id, value) values(2, 200)"));
+    }
+}
